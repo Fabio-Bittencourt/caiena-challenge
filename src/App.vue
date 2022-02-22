fdaa4ed2bec403e5d8bab571781a5b26603092a4
Fabio Bittencourt (eneas) <fabio@adimplere.com.br>
2022-02-22 09:43:06 -0300

    docs(tasks that needs to be done): things that i have to do

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@ import MenuBar from "./components/MenuBar.vue";
 import FooterBar from "./components/FooterBar.vue";
 import SearchPage from "./page/SearchPage.vue";
 
+//TODO Generate keys for access the github api
+//TODO Install vuex to handle stores and actions
+//TODO Services to handle the search of users
+//TODO Pagination allowing to change between 20, 30, 40, 50, 60, 100
+//TODO Unit Tests
+//TODO Inetgration Tests
+//TODO Components Tests
+
+//FIXME Improve exibition of users list (maybe)
+
 export default {
   components: {
     MenuBar,
