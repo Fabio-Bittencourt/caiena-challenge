e545dd2d032e77474215b1e77dc600df16ed89ff
Fabio Bittencourt (eneas) <fabio@adimplere.com.br>
2022-02-23 15:01:32 -0300

    fix: comunication with github api

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,22 +1,43 @@
 <script>
-import InputText from "../components/InputText.vue";
 import SubmitButton from "../components/SubmitButton.vue";
 
 export default {
   components: {
-    InputText,
     SubmitButton,
+  },
+  data() {
+    return {
+      search: "",
+      error: "",
+    };
+  },
+  methods: {
+    async onSubmit(param) {
+      try {
+        if (param !== "") {
+          this.$store.dispatch("getUsersBy", param);
+        }
+      } catch (e) {
+        console.log(":::", e);
+      }
+    },
   },
 };
 </script>
 
 <template>
   <div class="flex content-start">
-    <div class="w-full">
-      <InputText />
-    </div>
-    <div class="w-full">
-      <SubmitButton />
-    </div>
+    <form @submit.prevent="onSubmit(search)">
+      <div class="w-full">
+        <input
+          type="text"
+          class="w-full h-10 px-3 bg-gray-100 rounded-lg"
+          v-model="search"
+        />
+      </div>
+      <div class="w-full">
+        <SubmitButton />
+      </div>
+    </form>
   </div>
 </template>
